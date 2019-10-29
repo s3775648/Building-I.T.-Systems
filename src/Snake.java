4abce1c0e70f238821dc59f357a200e6d7800f58
Josh Coppen <s3775648@student.rmit.edu.au>
2019-10-29 23:25:55 +1030

    Added Extended Feature Additional Piece - Snake.java

---
+++ src/Snake.java
@@ -0,0 +1,34 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Snake extends Piece {
+
+    public Snake(boolean isWhite) {
+        super(isWhite, "Snake");
+    }
+
+    @Override
+    public int[][] canMove() {
+        int[][] moves = new int[][] {
+        	{1,1}, {1,3}, {3,1}, {3,3}, 		//North East
+        	{1,-1}, {1,-3}, {3,-1}, {3,-3}, 	//North West
+        	{-1,1}, {-1,3}, {-3,1}, {-3,3}, 	//South East
+        	{-1,-1}, {-1,-3}, {-3,-1}, {-3,-3}, // South West
+        };
+        return moves;
+    }
+
+    @Override
+    public BufferedImage pieceImage() throws IOException {
+        BufferedImage image;
+        if (isWhite()) {
+            image = ImageIO.read(getClass().getResource("Pieces/Final Pieces/White set/Whitesnake.png"));
+            return image;
+
+        } else {
+            image = ImageIO.read(getClass().getResource("Pieces/Final Pieces/Black set/Blacksnake.png"));
+            return image;
+        }
+    }
+}
