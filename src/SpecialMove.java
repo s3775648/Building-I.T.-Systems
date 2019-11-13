2af5c187a9e56214ace6a0e7fcb4aabfb74dcc4c
vinhdt90 <52366090+vinhdt90@users.noreply.github.com>
2019-11-13 14:23:10 +1100

    Added Pawn Evolution

--- src/SpecialMove.java
+++ src/SpecialMove.java
@@ -1,7 +1,59 @@
+import java.util.Scanner;
+
 public class SpecialMove {
 
-    public static Object[] castle(King king, Rook rook) throws IllegalCastlingException {
-        int result = checkCastlingLegal(king, rook);
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static int pawnEvolution(Pawn pawn, Game game) {
+        int result = checkPawnEvolution(pawn, game);
+        int selection = 0;
+        if (result == 1 || result == 2) {
+            while (selection == 0) {
+                System.out.println("Pawn Evolution Legal \n" +
+                        "1 - Pawn\n" +
+                        "2 - Rook\n" +
+                        "3 - Knight\n" +
+                        "4 - Bishop\n" +
+                        "5 - Queen\n");
+                selection = scanner.nextInt();
+                scanner.nextLine();
+            }
+        }
+        return selection;
+    }
+
+    private static int checkPawnEvolution(Pawn pawn, Game game) {
+
+        int[][] piecePosition = game.getPieceNumbers();
+
+        // White Pawn evolution
+        if (pawn.isWhite() && (piecePosition[0][0] == 1 ||
+                piecePosition[0][1] == 1 ||
+                piecePosition[0][2] == 1 ||
+                piecePosition[0][3] == 1 ||
+                piecePosition[0][4] == 1 ||
+                piecePosition[0][5] == 1 ||
+                piecePosition[0][6] == 1 ||
+                piecePosition[0][7] == 1)) {
+            return 1;
+        }
+        else if (!pawn.isWhite() && (piecePosition[7][0] == 1 ||
+                piecePosition[7][1] == 1 ||
+                piecePosition[7][2] == 1 ||
+                piecePosition[7][3] == 1 ||
+                piecePosition[7][4] == 1 ||
+                piecePosition[7][5] == 1 ||
+                piecePosition[7][6] == 1 ||
+                piecePosition[7][7] == 1)) {
+            return 2;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    public static Object[] castle(King king, Rook rook, Game game) throws IllegalCastlingException {
+        int result = checkCastlingLegal(king, rook, game);
         int[][] kingMovement, rookMovement;
         Object[] castlingMovements = new Object[1];
         
@@ -35,8 +87,8 @@ public class SpecialMove {
         return castlingMovements;    
     }
 
-    private static int checkCastlingLegal(King king, Rook rook) {
-        Game game = new Game();
+    private static int checkCastlingLegal(King king, Rook rook, Game game) {
+
         int[][] piecePosition = game.getPieceNumbers();
 
         // Black King castling WEST
